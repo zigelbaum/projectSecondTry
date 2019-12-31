cd2e19449f6cf23ce3ba724343939e8c8fc151ba
LeahBerkovits <leah1berkovits@gmail.com>
2019-12-31 15:40:02 +0200

    jjjj

---
+++ BL/ExceptionBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BL
+{
+    [Serializable]
+    internal class ExceptionBL : Exception
+    {
+        public ExceptionBL()
+        {
+        }
+
+        public ExceptionBL(string message) : base(message)
+        {
+        }
+
+        public ExceptionBL(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExceptionBL(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
