e9a2bffb13934287df1f87c602c4a8cef66509cb
chanush850@gmail.com <chanush850@gmail.com>
2020-01-04 21:01:51 +0200

    PLWpf

---
+++ PLWPF/GuestControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for GuestControl.xaml
+    /// </summary>
+    public partial class GuestControl : UserControl
+    {
+        private Calendar MyCalendar;
+        private Calendar CreateCalendar()
+        {
+            Calendar MonthlyCalendar = new Calendar();
+            MonthlyCalendar.Name = "MonthlyCalendar";
+            MonthlyCalendar.DisplayMode = CalendarMode.Month;
+            MonthlyCalendar.SelectionMode = CalendarSelectionMode.SingleRange;
+            MonthlyCalendar.IsTodayHighlighted = true;
+            return MonthlyCalendar;
+        }
+
+        private void SetBlackOutDates()
+        {
+            foreach (DateTime date in CurrentHostingUnit.AllOrders)
+            {
+                MyCalendar.BlackoutDates.Add(new CalendarDateRange(date));
+            }
+        }
+
+        public GuestControl()
+        {
+            MyCalendar = CreateCalendar();
+            vbCalendar.Child = null;
+            vbCalendar.Child = MyCalendar;
+            SetBlackOutDates();
+
+
+            InitializeComponent();
+        }
+
+
+
+    }
+}
---
+++ PLWPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BE;
+using BL;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IBL myBL;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myBL = FactoryBL.getBL("List");
+        }
+
+        private void HostinGUnitButton_Click(object sender, RoutedEventArgs e)
+        {
+            HostingUnitWindow unitWindow = new HostingUnitWindow();
+            unitWindow.ShowDialog();
+        }
+
+        private void GuestRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            GuestRequestWindow requestWindow = new GuestRequestWindow();
+            requestWindow.ShowDialog();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderWindow orderWindow = new OrderWindow();
+            orderWindow.ShowDialog();
+        }
+    }
+}
