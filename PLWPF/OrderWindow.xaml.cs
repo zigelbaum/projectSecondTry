e6b3a099b5b9ef7f4e9731a8f502305cd3040126
LeahBerkovits <leah1berkovits@gmail.com>
2020-01-06 18:44:26 +0200

    startOrderWindow

--- PLWPF/OrderWindow.xaml.cs
+++ PLWPF/OrderWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BE;
+using BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace PLWPF
 {
     /// <summary>
@@ -19,8 +22,21 @@ namespace PLWPF
     /// </summary>
     public partial class OrderWindow : Window
     {
+        IBL myBl = BL.FactoryBL.getBL("XML");
+        HostingUnit host; // לעשות שהוא יבין לבד לאיזו יחידה לבדוק התאמה
+        public List<GuestRequest> matchRequests;
+        List<Order> listOrders;
         public OrderWindow()
         {
+            IEnumerable<IGrouping<Host, HostingUnit>> my_units = myBl.GroupHostByHostingUnit();
+            foreach (IGrouping<Host, HostingUnit> hosting in my_units)
+            {
+                foreach (HostingUnit unit in hosting)
+                {
+                    matchRequests = myBl.RequestMatchToStipulation(myBl.BuildPredicate(unit));
+                }
+            }            
+            listOrders = myBl.getOrders(u => u.HostingUnitKey == host.HostingUnitKey);
             InitializeComponent();
         }
     }
