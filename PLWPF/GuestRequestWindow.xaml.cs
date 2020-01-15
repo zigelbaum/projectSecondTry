e79a8cde0872dc1f3512772099d225e3bc68b2e4
zigelbaum <Chanush850@gmail.com>
2020-01-15 21:35:04 +0200

    allmost done

--- PLWPF/GuestRequestWindow.xaml.cs
+++ PLWPF/GuestRequestWindow.xaml.cs
@@ -32,9 +32,10 @@ namespace PLWPF
         public GuestRequestWindow()
         {
             InitializeComponent();
-            requestView.ItemsSource = requestsList;
+            listToFilter = requestsList;
+            requestView.ItemsSource =listToFilter ;
             cbbGroupBy.ItemsSource = Enum.GetValues(typeof(filterRequest));
-            cbbGroupBy.SelectedIndex = 1;
+            cbbGroupBy.SelectedIndex = 0;
             cbbShowGroup.IsEnabled = false;
         }
 
@@ -64,7 +65,61 @@ namespace PLWPF
 
         private void tbKey_SearchFilterChanged(object sender, TextChangedEventArgs e)
         {
+            if (tbKey.Text != null)
+            {
+                string searchTxt = tbKey.Text;
+                string upper = searchTxt.ToUpper();
+                string lower = searchTxt.ToLower();
 
+                var request = from item in listToFilter
+                              let key = item.GuestRequestKey.ToString()
+                              where
+                              key.StartsWith(lower)
+                              || key.StartsWith(upper)
+                              || key.Contains(searchTxt)
+                              select item;
+                requestView.ItemsSource = request;
+            }
+        }
+
+        private void tbMail_SearchFilterChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbMail.Text!=null)
+            {
+                string searchTxt = tbMail.Text;
+                string upper = searchTxt.ToUpper();
+                string lower = searchTxt.ToLower();
+
+                var request = from item in listToFilter
+                              let key = item.MailAddress
+                              where
+                              key.StartsWith(lower)
+                              || key.StartsWith(upper)
+                              || key.Contains(searchTxt)
+                              select item;
+
+                requestView.ItemsSource = request;
+            }
+        }
+
+        private void tbLastName_SearchFilterChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbLastName.Text!=null)
+            {
+                string searchTxt = tbLastName.Text;
+                string upper =searchTxt.ToUpper();
+                string lower = searchTxt.ToLower();
+
+                var request = from item in listToFilter
+                              let key = item.FamilyName
+                              where
+                              key.StartsWith(lower)
+                              || key.StartsWith(upper)
+                              || key.Contains(searchTxt)
+                              select item;
+
+                requestView.ItemsSource = request;
+            }
         }
 
         private void cbbGroupBy_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -73,7 +128,7 @@ namespace PLWPF
             switch (group)
             {
                 case 0:
-                    //dont know if works
+                    cbbShowGroup.SelectedItem = null;
                     cbbShowGroup.IsEnabled = false;
                     requestsList = new ObservableCollection<GuestRequest>(MainWindow.myBL.GetGuestRequestsList());
                     requestView.ItemsSource = requestsList;
@@ -83,26 +138,32 @@ namespace PLWPF
                     Enums.GuestRequestStatus[] keysOfstatus = (from item in groupedByStatus select item.Key).ToArray();
                     cbbShowGroup.ItemsSource = keysOfstatus;
                     cbbShowGroup.IsEnabled = true;
+                    cbbShowGroup.SelectedItem = 0;
                     break;
                 case 2:
                     groupedByArea = new ObservableCollection<IGrouping<Enums.Area, GuestRequest>>(MainWindow.myBL.GroupGRByArea());
                     Enums.Area[] keysOfArea = (from item in groupedByArea select item.Key).ToArray();
                     cbbShowGroup.ItemsSource = keysOfArea;
                     cbbShowGroup.IsEnabled = true;
+                    cbbShowGroup.SelectedItem = 0;
                     break;
                 case 3:
                     groupedByType = new ObservableCollection<IGrouping<Enums.HostingUnitType, GuestRequest>>(MainWindow.myBL.GroupGRByType());
                     Enums.HostingUnitType[] keysOfType = (from item in groupedByType select item.Key).ToArray();
                     cbbShowGroup.ItemsSource = keysOfType;
                     cbbShowGroup.IsEnabled = true;
+                    cbbShowGroup.SelectedItem = 0;
                     break;
                 case 4:
                     groupedByStars = new ObservableCollection<IGrouping<int, GuestRequest>>(MainWindow.myBL.GroupGRByStars());
                     int[] keysOfStars = (from item in groupedByStars select item.Key).ToArray();
                     cbbShowGroup.ItemsSource = keysOfStars;
                     cbbShowGroup.IsEnabled = true;
+                    cbbShowGroup.SelectedItem = 0;
                     break;
             }
+
+
         }
 
         private void cbbShowGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -150,6 +211,18 @@ namespace PLWPF
                     break;
             }
             requestView.ItemsSource = listToFilter;
+            if ((tbKey.Text.Length != 0 || tbMail.Text.Length != 0 || tbLastName.Text.Length != 0) && listToFilter != null)
+                tbKey_SearchFilterChanged(null, null);
+        }
+       
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            listToFilter = requestsList;
+            requestView.ItemsSource = listToFilter;
+        }
+
+        private void viewRequest(object sender, MouseButtonEventArgs e)
+        {
 
         }
     }
